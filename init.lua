3de1454480e7d29b1f11026fce82f6e859f105e3
brandeschi <contact.brandontedeschi@gmail.com>
2023-06-15 13:07:33 -0400

    update(nullls): use diag with eslint_d

--- init.lua
+++ init.lua
@@ -193,6 +193,7 @@ require("lazy").setup({
                 sources = {
                     null_ls.builtins.formatting.prettierd,
                     null_ls.builtins.code_actions.eslint_d,
+                    null_ls.builtins.diagnostics.eslint_d,
                     null_ls.builtins.formatting.black
                 },
                 on_attach = function(client, bufnr)
