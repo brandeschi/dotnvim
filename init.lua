ef131978db727a7dd89278b7a64e5f14e8340933
brandeschi <contact.brandontedeschi@gmail.com>
2023-11-10 09:48:35 -0500

    change null-ls eslint

--- init.lua
+++ init.lua
@@ -192,8 +192,10 @@ require("lazy").setup({
             null_ls.setup({
                 sources = {
                     null_ls.builtins.formatting.prettierd,
-                    null_ls.builtins.code_actions.eslint_d,
-                    null_ls.builtins.diagnostics.eslint_d,
+                    -- null_ls.builtins.code_actions.eslint_d,
+                    -- null_ls.builtins.diagnostics.eslint_d,
+                    null_ls.builtins.code_actions.eslint,
+                    null_ls.builtins.diagnostics.eslint,
                     null_ls.builtins.formatting.black
                 },
                 on_attach = function(client, bufnr)
